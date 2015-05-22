5676fea40b5166152ad82a721642e79d4e3cff91
Iliya Iliev <iliya.vasilev.iliev@gmail.com>
2015-05-22 21:29:00 +0300

    add price packets

--- js/controllers/childrenCtrl.js
+++ js/controllers/childrenCtrl.js
@@ -4,14 +4,17 @@ app.controller('childrenCtrl', function ($scope) {
         $("#menu").show();
         // Set of Photos
         $scope.photos = [
-            {src: 'images/background/3.jpg', desc: 'Image 01'},
-            {src: 'images/background/3.jpg', desc: 'Image 01'},
-            {src: 'images/background/3.jpg', desc: 'Image 01'},
-            {src: 'images/background/3.jpg', desc: 'Image 01'},
-            {src: 'images/background/3.jpg', desc: 'Image 01'},
-            {src: 'images/background/3.jpg', desc: 'Image 01'},
-            {src: 'images/background/3.jpg', desc: 'Image 01'},
-            {src: 'images/background/3.jpg', desc: 'Image 01'}
+            {src: 'images/children/1.jpg'},
+            {src: 'images/children/2.jpg'},
+            {src: 'images/children/3.jpg'},
+            {src: 'images/children/4.jpg'},
+            {src: 'images/children/5.jpg'},
+            {src: 'images/children/6.jpg'},
+            {src: 'images/children/7.jpg'},
+            {src: 'images/children/8.jpg'},
+            {src: 'images/children/9.jpg'},
+            {src: 'images/children/10.jpg'},
+            {src: 'images/children/11.jpg'}
         ];
 
         // initial image index
--- js/controllers/menCtrl.js
+++ js/controllers/menCtrl.js
@@ -4,16 +4,17 @@ app.controller('menCtrl', function ($scope) {
         $("#menu").show();
         // Set of Photos
         $scope.photos = [
-            {src: 'images/background/2.jpg', desc: 'Image 01'},
-            {src: 'http://farm9.staticflickr.com/8449/7918424278_4835c85e7a_b.jpg', desc: 'Image 02'},
-            {src: 'http://farm9.staticflickr.com/8457/7918424412_bb641455c7_b.jpg', desc: 'Image 03'},
-            {src: 'http://farm9.staticflickr.com/8179/7918424842_c79f7e345c_b.jpg', desc: 'Image 04'},
-            {src: 'http://farm9.staticflickr.com/8315/7918425138_b739f0df53_b.jpg', desc: 'Image 05'},
-            {src: 'http://farm9.staticflickr.com/8449/7918424278_4835c85e7a_b.jpg', desc: 'Image 06'},
-            {src: 'http://farm9.staticflickr.com/8457/7918424412_bb641455c7_b.jpg', desc: 'Image 07'},
-            {src: 'http://farm9.staticflickr.com/8179/7918424842_c79f7e345c_b.jpg', desc: 'Image 08'},
-            {src: 'http://farm9.staticflickr.com/8315/7918425138_b739f0df53_b.jpg', desc: 'Image 09'},
-            {src: 'http://farm9.staticflickr.com/8461/7918425364_fe6753aa75_b.jpg', desc: 'Image 10'}
+            {src: 'images/men/1.jpg'},
+            {src: 'images/men/2.jpg'},
+            {src: 'images/men/3.jpg'},
+            {src: 'images/men/4.jpg'},
+            {src: 'images/men/5.jpg'},
+            {src: 'images/men/6.jpg'},
+            {src: 'images/men/7.jpg'},
+            {src: 'images/men/8.jpg'},
+            {src: 'images/men/9.jpg'},
+            {src: 'images/men/10.jpg'},
+            {src: 'images/men/11.jpg'}
         ];
 
         // initial image index
